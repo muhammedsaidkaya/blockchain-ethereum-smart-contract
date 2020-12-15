e2501fc7508ca97fc84e477aadf7a88c5848e0d6
b21627428 <b21627428@cs.hacettepe.edu.tr>
2020-12-16 00:42:35 +0300

    contract updated.

--- taxi.sol
+++ taxi.sol
@@ -3,16 +3,17 @@ pragma solidity 0.6.11;
 contract TaxiInvestmentContract{
     
     //Structs
-    struct Participant{
-        address payable investor;
-        uint balance;
-    }
+
     struct Car{
         uint32 carID;
         uint price;
     }
+    struct Participant{
+        address payable addr;
+        uint balance;
+    }    
     struct TaxiDriver{
-        address payable driver;
+        address payable addr;
         uint balance;
         uint salary;
         uint lastPayment;
@@ -21,19 +22,25 @@ contract TaxiInvestmentContract{
         address payable addr;
         uint lastPayment;
     }
+
+    
+    
+    struct Proposal{
+        uint approvalNumber;
+        mapping(address=>bool) votes;
+    }
     struct ProposedCar{
         Car car;
         uint offerValidTime;
     }
     struct CarPurchasingProposal{
+        Proposal proposal;
         ProposedCar proposedCar;
-        uint approvalNumber;
-        mapping(address=>bool) votes;
     }
     struct DriverProposal{
-        TaxiDriver taxiDriver;
-        uint approvalNumber;
-        mapping(address=>bool) votes;
+        Proposal proposal;
+        TaxiDriver proposedTaxiDriver;
+
     }
     
     
@@ -48,104 +55,188 @@ contract TaxiInvestmentContract{
     address[] participantsAddresses;
     mapping(address=>Participant) public participants;
     
-    CarPurchasingProposal purchaseProposal;
-    CarPurchasingProposal rePurchaseProposal;
+    CarPurchasingProposal carPurchaseProposal;
+    CarPurchasingProposal carRePurchaseProposal;
     DriverProposal taxiDriverProposal;
+
     
     //Constructor
     constructor() public {
         manager = msg.sender;
     }
     
-    
-    
 
     
     
-    //Modifiers
     modifier isNotMaxParticipantSize() {
-        require(participantsAddresses.length != 9);
+        require(participantsAddresses.length != 9,"Participant size already 9.");
         _;
     }
     modifier isParticipationFeeEnough(){
-        require(msg.value == 100 ether);
+        require(msg.value == 100 ether,"In order to join , Value must be 100 ether.");
         _;
     }
-    modifier isManager(){
-        require(msg.sender == manager);
+    modifier isRightAddress(address _addr,string memory _desc){
+        require(msg.sender == _addr,_desc);
         _;
     }
-    modifier isCarDealer(){
-        require(msg.sender == carDealer.addr);
+    modifier isEnougYesVoteForProposal(Proposal memory _proposal){
+        require(participantsAddresses.length/2 < _proposal.approvalNumber,"Yes vote number is not enough.");
         _;
     }
-    modifier isParticipant(){
-        require(msg.sender == participants[msg.sender].investor,"You are not participant.");
+    modifier isLessThan(uint _temp1,uint _temp2,bool _equality,string memory _desc){
+        if(_equality)
+            require(_temp1 <= _temp2,_desc);
+        else
+            require(_temp1 < _temp2,_desc);
         _;
     }
-    modifier isTaxiDriver(){
-        require(msg.sender == taxiDriver.driver);
+    modifier isSecondPassedAfterLastPayment(uint _time,uint _lastPayment){
+        require(_time < now - _lastPayment,"The time not passed after last payment.");
         _;
     }
-    modifier isOfferValidTime(CarPurchasingProposal memory proposal){
-        require(proposal.proposedCar.offerValidTime >= now);
-        _;
-    }
-    modifier isParticipantNotAlreadyVotedOnProposal(CarPurchasingProposal storage proposal){
-        require(!proposal.votes[msg.sender]);
-        _;
-    }
-    modifier isParticipantNotAlreadyVotedOnDriverProposal(){
-        require(!taxiDriverProposal.votes[msg.sender]);
-        _;
-    }
-    modifier isEnoughMoneyForPurchaseCar(CarPurchasingProposal memory proposal){
-        require(proposal.proposedCar.car.price <= address(this).balance);
-        _;
-    }
-    modifier isEnoughYesVoteForPurchaseCar(CarPurchasingProposal memory proposal){
-        require(participantsAddresses.length/2 < proposal.approvalNumber);
-        _;
-    }
-    modifier isEnougYesVoteForDriver(){
-        require(participantsAddresses.length/2 < taxiDriverProposal.approvalNumber);
-        _;
-    }
-    modifier isGreaterThanZero(uint value){
-        require(value > 0);
-        _;
-    }
-    modifier isOneMonthPassedAfterDriversLastPayment(){
-        require(60 * 60 * 24 * 30 < now - taxiDriver.lastPayment);
-        _;
-    }
-    modifier isSixMonthPassedAfterCarDealerPayment(){
-        require(60 * 60 * 24 * 30 * 6 < now - carDealer.lastPayment);
-        _;
-    }
-    modifier isSixMonthPassedAfterPayDividend(){
-        require(now - managerPayDividendTime > 60 * 60 * 24 * 30 * 6 );
-        _;
+
+
+
+    
+    function getTotalBalance() public view returns(uint){
+        return address(this).balance;
     }
     
-    //Private Functions
-    function getTime() private view returns(uint32){
-        return uint32(now);
+    function join() payable public isNotMaxParticipantSize isParticipationFeeEnough{
+        participants[msg.sender] = Participant(msg.sender,0);
+        participantsAddresses.push(msg.sender);
     }
-    function incrementYesVoteCount(CarPurchasingProposal memory proposal) private pure {
-        proposal.approvalNumber += 1;
+    
+    
+    
+    
+    
+    function proposeDriver(address payable _addr, uint _price) public isRightAddress(manager,"This function called by manager."){
+        taxiDriverProposal = DriverProposal({
+            proposal : Proposal({
+                approvalNumber:0
+            }),
+            proposedTaxiDriver: TaxiDriver({
+               addr : _addr,
+               balance : 0,
+               salary : _price,
+               lastPayment : 0
+           })
+        });
     }
-    function creteaCarPurchasingProposal(uint32 _carID,uint _price,uint32 _offerValidTime) private pure returns(CarPurchasingProposal memory){
-        return CarPurchasingProposal({
+    function carProposeToBusiness(uint32 _carID,uint _price,uint _offerValidTime) public isRightAddress(carDealer.addr,"This function called by car dealer."){
+        carPurchaseProposal = CarPurchasingProposal({
+            proposal : Proposal({
+                approvalNumber:0
+            }),
             proposedCar: ProposedCar({
                 car: Car({
                     carID : _carID,
                     price : _price
                 }),
                 offerValidTime : _offerValidTime
-            }),
-            approvalNumber : 0
+            })
         });
+    }
+    function rePurchaseCarPropose(uint32 _carID,uint _price,uint _offerValidTime) public isRightAddress(carDealer.addr,"This function called by car dealer."){
+        carRePurchaseProposal = CarPurchasingProposal({
+            proposal : Proposal({
+                approvalNumber:0
+            }),
+            proposedCar: ProposedCar({
+                car: Car({
+                    carID : _carID,
+                    price : _price
+                }),
+                offerValidTime : _offerValidTime
+            })
+        });
+    }
+
+
+
+
+    function setCarDealer(address payable _addr) public isRightAddress(manager,"This function called by manager."){
+        carDealer = CarDealer({
+            addr : _addr,
+            lastPayment : 0
+        });
+    }
+    function setDriver() public isRightAddress(manager,"This function called by manager.") isEnougYesVoteForProposal(taxiDriverProposal.proposal){
+        taxiDriver = taxiDriverProposal.proposedTaxiDriver;
+    }
+    
+    
+    
+    
+    function approvePurchaseCar() public isRightAddress(participants[msg.sender].addr,"This function called by participant.") isLessThan(now , carPurchaseProposal.proposedCar.offerValidTime , true,"Offer valid time passed."){
+        require(!carPurchaseProposal.proposal.votes[msg.sender]);
+        carPurchaseProposal.proposal.votes[msg.sender] = true;
+        carPurchaseProposal.proposal.approvalNumber += 1;
+    }
+    function approveSellProposal() public isRightAddress(participants[msg.sender].addr,"This function called by participant.") isLessThan(now , carRePurchaseProposal.proposedCar.offerValidTime , true,"Offer valid time passed."){
+        require(!carRePurchaseProposal.proposal.votes[msg.sender]);
+        carRePurchaseProposal.proposal.votes[msg.sender] = true;
+        carRePurchaseProposal.proposal.approvalNumber += 1;
+    }
+    function approveDriver() public isRightAddress(participants[msg.sender].addr,"This function called by participant.") {
+        require(!taxiDriverProposal.proposal.votes[msg.sender]);
+        taxiDriverProposal.proposal.votes[msg.sender] = true;
+        taxiDriverProposal.proposal.approvalNumber += 1;
+    }
+    
+    
+    
+    function purchaseCar() public  isRightAddress(manager,"This function called by manager.") isLessThan(carPurchaseProposal.proposedCar.car.price , address(this).balance , true,"Contract balance is not enough.") isEnougYesVoteForProposal(carPurchaseProposal.proposal){
+        Car memory temp = carPurchaseProposal.proposedCar.car;
+        carDealer.addr.transfer(temp.price);
+        ownedCar = temp.carID;
+        delete carPurchaseProposal;
+    }
+    function rePurchaseCar() payable public isRightAddress(carDealer.addr,"This function called by car dealer.") isLessThan(carRePurchaseProposal.proposedCar.car.price , msg.value, true,"The paid value is not enough for car price.") isEnougYesVoteForProposal(carRePurchaseProposal.proposal) {
+        ownedCar = 0;
+        delete carRePurchaseProposal;
+    }
+
+    
+    function payTaxiCharge() public payable isLessThan(0,msg.value,false,"Taxi charge money must be greater than zero."){}
+
+
+
+
+    function fireDriver() public isRightAddress(manager,"This function called by manager.") {
+        taxiDriver.addr.transfer(taxiDriver.salary);
+        delete taxiDriver;
+    }
+    function releaseSalary() public isRightAddress(manager,"This function called by manager.") isSecondPassedAfterLastPayment(60 * 60 * 24 * 30,taxiDriver.lastPayment){
+        taxiDriver.balance += taxiDriver.salary;
+        taxiDriver.lastPayment = uint(now);
+    }
+    function getSalary() public isRightAddress(taxiDriver.addr,"This function called by taxi driver.") isLessThan(0,taxiDriver.balance,false,"Taxi Balance must be greater than zero."){
+        taxiDriver.addr.transfer(taxiDriver.balance);
+        taxiDriver.balance = 0;
+    }
+    
+    
+    
+    
+    
+    function payCarExpenses() public isRightAddress(manager,"This function called by manager.") isSecondPassedAfterLastPayment(60 * 60 * 24 * 30 * 6,carDealer.lastPayment){
+        carDealer.addr.transfer(10 ether);
+        carDealer.lastPayment = uint(now);
+    }
+    
+    
+    
+    
+    function payDividend() public isRightAddress(manager,"This function called by manager.") isSecondPassedAfterLastPayment(60 * 60 * 24 * 30 * 6,managerPayDividendTime){
+        uint amount = decreaseCarDealerMoney(decreaseDriverMoney(address(this).balance));
+        for(uint i=0; i< participantsAddresses.length ; i++){
+            address temp = participantsAddresses[i];
+            participants[temp].balance += amount/ participantsAddresses.length;
+        }
+        managerPayDividendTime = uint(now);
     }
     function decreaseDriverMoney(uint _amount) private view returns(uint){
         uint oneMonth = 60 * 60 * 24 * 30;
@@ -157,109 +248,14 @@ contract TaxiInvestmentContract{
         if(now - carDealer.lastPayment > sixMonth) _amount -= 10 ether;
         return _amount;
     } 
-    
-    
-    //Public Functions
-    
-    function getTotalBalance() public view returns(uint){
-        return address(this).balance;
-    }
-    
-    //Joining
-    function join() payable public isNotMaxParticipantSize isParticipationFeeEnough{
-        participants[msg.sender] = Participant(msg.sender,0);
-        participantsAddresses.push(msg.sender);
-    }
-    
-    //Sets
-    function setCarDealer(address payable _carDealersAddress) public isManager{
-        carDealer = CarDealer({
-            addr:_carDealersAddress,
-            lastPayment : 0
-        });
-    }
-    function setDriver() public isManager isEnougYesVoteForDriver{
-        taxiDriver = taxiDriverProposal.taxiDriver;
-    }
-    
-    
-    //Proposes
-    function carProposeToBusiness(uint32 _carID,uint _price,uint32 _offerValidTime) public isCarDealer{
-        purchaseProposal = creteaCarPurchasingProposal(_carID,_price,_offerValidTime);
-    }
-    function rePurchaseCarPropose(uint32 _carID,uint _price,uint32 _offerValidTime) public isCarDealer{
-        rePurchaseProposal = creteaCarPurchasingProposal(_carID,_price,_offerValidTime);
-    }
-    function proposeDriver(address payable _taxiDriver, uint _price) public isManager{
-        taxiDriverProposal = DriverProposal({
-           taxiDriver: TaxiDriver({
-               driver : _taxiDriver,
-               balance : 0,
-               salary : _price,
-               lastPayment : 0
-           }),
-           approvalNumber : 0
-        });
-    }
-    
-    
-    //Approves
-    function approvePurchaseCar() public isParticipant isOfferValidTime(purchaseProposal) isParticipantNotAlreadyVotedOnProposal(purchaseProposal){
-        purchaseProposal.votes[msg.sender] = true;
-        incrementYesVoteCount(purchaseProposal);
-    }
-    function approveSellProposal() public isParticipant isOfferValidTime(rePurchaseProposal) isParticipantNotAlreadyVotedOnProposal(rePurchaseProposal){
-        rePurchaseProposal.votes[msg.sender] = true;
-        incrementYesVoteCount(rePurchaseProposal);
-    }
-    function approveDriver() public isParticipant isParticipantNotAlreadyVotedOnDriverProposal{
-        taxiDriverProposal.votes[msg.sender] = true;
-        taxiDriverProposal.approvalNumber += 1;
-    }
-    
-    
-    //Payables
-    function purchaseCar() public isEnoughMoneyForPurchaseCar(purchaseProposal) isEnoughYesVoteForPurchaseCar(purchaseProposal) isManager{
-        Car memory temp = purchaseProposal.proposedCar.car;
-        carDealer.addr.transfer(temp.price);
-        ownedCar = temp.carID;
-        delete purchaseProposal;
-    }
-    function rePurchaseCar() payable public isEnoughMoneyForPurchaseCar(rePurchaseProposal) isEnoughYesVoteForPurchaseCar(rePurchaseProposal) isCarDealer{
-        ownedCar = 0;
-        delete rePurchaseProposal;
-    }
-    function payTaxiCharge() public payable isGreaterThanZero(msg.value){}
-    function fireDriver() public isManager {
-        taxiDriver.driver.transfer(taxiDriver.salary);
-        delete taxiDriver;
-    }
-    function releaseSalary() public isManager isOneMonthPassedAfterDriversLastPayment{
-        taxiDriver.balance += taxiDriver.salary;
-        taxiDriver.lastPayment = uint(now);
-    }
-    function getSalary() public isTaxiDriver isGreaterThanZero(taxiDriver.balance){
-        taxiDriver.driver.transfer(taxiDriver.balance);
-        taxiDriver.balance = 0;
-    }
-    function payCarExpenses() public isManager isSixMonthPassedAfterCarDealerPayment{
-        carDealer.addr.transfer(10 ether);
-        carDealer.lastPayment = uint(now);
-    }
-    function payDividend() public isManager isSixMonthPassedAfterPayDividend{
-        uint amount = decreaseCarDealerMoney(decreaseDriverMoney(address(this).balance));
-        for(uint i=0; i< participantsAddresses.length ; i++){
-            address temp = participantsAddresses[i];
-            participants[temp].balance += amount/ participantsAddresses.length;
-        }
-        managerPayDividendTime = uint(now);
-    }
-    function getDividend() public isParticipant{
-        assert(address(this).balance >= participants[msg.sender].balance);
-        participants[msg.sender].investor.transfer(participants[msg.sender].balance);
+    function getDividend() public isRightAddress(participants[msg.sender].addr,"This function called by participant.") isLessThan(participants[msg.sender].balance , address(this).balance , true,"Contract balance is not enough."){
+        participants[msg.sender].addr.transfer(participants[msg.sender].balance);
         participants[msg.sender].balance = 0;
     }
-    
+
+
+
+
     fallback () external {
         
     }
